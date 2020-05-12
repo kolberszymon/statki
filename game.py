db879ca4a345a359086f7cfc5983b16beef432e6
Szymon Kolber <kolberszymon@gmail.com>
2020-05-12 17:18:42 +0200

    Can place ships

--- game.py
+++ game.py
@@ -27,12 +27,6 @@ class Game():
     def __init__(self, player1, player2):
         self.players = [player1, player2]
         self.active_player = 0
-        self.ships = {
-            4: 1,
-            3: 2,
-            2: 3,
-            1: 5
-        }
 
     def start_conf(self):
         self.players[self.active_player].is_active = 1
@@ -84,66 +78,24 @@ class Player():
             self.picked_square_in_width = self.picked_square_in_width + width
         if self.picked_square_in_height + height >= 0 and self.picked_square_in_height + height < self.board.dimension:
             self.picked_square_in_height = self.picked_square_in_height + height
-        self.draw_border()
-
-    def check_if_can_place_ship(self, is_placing_started, whole_ship_position = []):
-        can_place = True
-        if is_placing_started == 0:
-            for ship in self.ships_places:
-                if ship[0] == self.picked_square_in_width - 1 and ship[1] == self.picked_square_in_height - 1:
-                    can_place = False
-                elif ship[0] == self.picked_square_in_width and ship[1] == self.picked_square_in_height - 1:
-                    can_place = False
-                elif ship[0] == self.picked_square_in_width + 1 and ship[1] == self.picked_square_in_height - 1:
-                    can_place = False
-                elif ship[0] == self.picked_square_in_width - 1 and ship[1] == self.picked_square_in_height:
-                    can_place = False
-                elif ship[0] == self.picked_square_in_width and ship[1] == self.picked_square_in_height:
-                    can_place = False
-                elif ship[0] == self.picked_square_in_width + 1 and ship[1] == self.picked_square_in_height:
-                    can_place = False
-                elif ship[0] == self.picked_square_in_width - 1 and ship[1] == self.picked_square_in_height + 1:
-                    can_place = False
-                elif ship[0] == self.picked_square_in_width and ship[1] == self.picked_square_in_height + 1:
-                    can_place = False
-                elif ship[0] == self.picked_square_in_width + 1  and ship[1] == self.picked_square_in_height + 1:
-                    can_place = False
-
-        return can_place
-
-
-    def draw_board(self):
-        self.board.draw_board()
-
-    def draw_ships(self):
-        self.board.draw_ships(self.ships_places)
-
-    def draw_border(self):
-        if self.game_phase == 1:
-            can_place = self.check_if_can_place_ship(0)
-            self.board.draw_border(self.picked_square_in_width, self.picked_square_in_height, can_place)
+        self.draw_all()
 
     def draw_cross(self):
-        self.board.draw_img(crossImg ,self.picked_square_in_width, self.picked_square_in_height)
+        self.board.draw_action(crossImg ,self.picked_square_in_width, self.picked_square_in_height)
 
     def draw_ship(self):
-        self.board.draw_img(shipImg, self.picked_square_in_width, self.picked_square_in_height)
+        self.board.draw_action(shipImg, self.picked_square_in_width, self.picked_square_in_height)
 
     def place_ship(self):
-        placed = 0
-
-        can_place = self.check_if_can_place_ship(0)
-        if can_place:
-            self.ships_places.append([self.picked_square_in_width, self.picked_square_in_height])
-
-
+        self.board.place_ship(self.picked_square_in_width, self.picked_square_in_height)
 
     def draw_all(self):
+        self.board.draw_all(self.picked_square_in_width, self.picked_square_in_height)
 
-        if self.is_active:
-            self.draw_border_on_self()
-        self.draw_board()
-        self.draw_ships()
+
+
+
+
 ############# BOARD
 
 class Board():
@@ -161,10 +113,19 @@ class Board():
 
         # Ships info
 
-        self.ships_postion = []
+        self.ships_position = []
+        self.current_ship_position = []
         self.shots_hit_position = []
         self.shots_missed_position = []
+        self.placed_ships = 0
+        self.placed_ships_figure = 0
+        self.is_ship_being_placed = False
+        self.ship_sizes = [4, 3, 3, 2, 2, 2, 1, 1, 1, 1, 1]
 
+
+        # Moves info
+
+        self.game_phase = 1
 
 
     def draw_board(self):
@@ -176,19 +137,19 @@ class Board():
 
                 pygame.draw.rect(screen, (255, 255, 255), (square_x_pos, square_y_pos, self.square_width, self.square_height))
 
-    def draw_ships(self, ships_places):
-        for ship in ships_places:
+    def draw_ships(self):
+        for ship in self.ships_position:
             for square_num_width in range(self.dimension):
                 for square_num_height in range(self.dimension):
                     if ship[0] == square_num_width and ship[1] == square_num_height:
-                        self.draw_img(shipImg, square_num_width, square_num_height)
+                        self.draw_action(shipImg, square_num_width, square_num_height)
 
     def draw_border(self, sq_width_num, sq_height_num, valid):
 
-        color = (255, 0, 0)
+        color = (255, 0, 0) #Red
 
         if valid is True:
-            color = (0, 255, 0)
+            color = (0, 255, 0) #Green
 
         square_num_width = sq_width_num
         square_num_height = sq_height_num
@@ -214,6 +175,68 @@ class Board():
 
         screen.blit(img, (square_x_pos,square_y_pos))
 
+    def draw_all(self, picked_square_in_width, picked_square_in_height):
+        if self.game_phase == 1:
+            self.draw_border(picked_square_in_width, picked_square_in_height, self.check_if_move_is_valid(picked_square_in_width, picked_square_in_height))
+            self.draw_board()
+            self.draw_ships()
+            self.draw_action(shipImg, picked_square_in_width, picked_square_in_height)
+
+
+
+
+    def place_ship(self, picked_square_in_width, picked_square_in_height):
+        if self.check_if_move_is_valid(picked_square_in_width, picked_square_in_height):
+            self.ships_position.append([picked_square_in_width, picked_square_in_height])
+            self.current_ship_position.append([picked_square_in_width, picked_square_in_height])
+            self.is_ship_being_placed = True
+            self.placed_ships += 1
+
+            if self.placed_ships == self.ship_sizes[self.placed_ships_figure]:
+                self.is_ship_being_placed = False
+                self.placed_ships = 0
+                self.placed_ships_figure += 1
+
+
+    def check_if_move_is_valid(self, picked_square_in_width, picked_square_in_height):
+        if self.game_phase == 1:
+            if self.is_ship_being_placed == False:
+                can_place = True
+                for ship in self.ships_position:
+                    if ship[0] == picked_square_in_width - 1 and ship[1] == picked_square_in_height - 1:
+                        can_place = False
+                    elif ship[0] == picked_square_in_width and ship[1] == picked_square_in_height - 1:
+                        can_place = False
+                    elif ship[0] == picked_square_in_width + 1 and ship[1] == picked_square_in_height - 1:
+                        can_place = False
+                    elif ship[0] == picked_square_in_width - 1 and ship[1] == picked_square_in_height:
+                        can_place = False
+                    elif ship[0] == picked_square_in_width and ship[1] == picked_square_in_height:
+                        can_place = False
+                    elif ship[0] == picked_square_in_width + 1 and ship[1] == picked_square_in_height:
+                        can_place = False
+                    elif ship[0] == picked_square_in_width - 1 and ship[1] == picked_square_in_height + 1:
+                        can_place = False
+                    elif ship[0] == picked_square_in_width and ship[1] == picked_square_in_height + 1:
+                        can_place = False
+                    elif ship[0] == picked_square_in_width + 1  and ship[1] == picked_square_in_height + 1:
+                        can_place = False
+            elif self.is_ship_being_placed == True:
+                can_place = True
+                for ship in self.current_ship_position:
+                    if ship[0] == picked_square_in_width and ship[1] == picked_square_in_height:
+                        can_place = False
+                    elif ship[0] == picked_square_in_width - 1 and ship[1] == picked_square_in_height - 1:
+                        can_place = False
+                    elif ship[0] == picked_square_in_width + 1 and ship[1] == picked_square_in_height + 1:
+                        can_place = False
+                    elif ship[0] == picked_square_in_width - 1 and ship[1] == picked_square_in_height + 1:
+                        can_place = False
+                    elif ship[0] == picked_square_in_width + 1 and ship[1] == picked_square_in_height - 1:
+                        can_place = False
+
+            return can_place
+
 
 
 
@@ -229,11 +252,7 @@ while not done:
     screen.fill((0,0,0))
 
     active_player = game.players[game.active_player]
-    active_player.draw_border_on_self()
-
-    game.draw_boards()
-
-    active_player.draw_ship()
+    active_player.draw_all()
 
     for event in pygame.event.get():
                 if event.type == pygame.QUIT:
